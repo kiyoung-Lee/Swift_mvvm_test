7485f535ab267bfd60acfaf6b2152c45e22a2c5c
Kiyoung Lee <eevee300@gmail.com>
2017-07-06 20:49:58 +0900

    add StarExplanation Project

---
+++ StartExplnation/ConstellationExplanation/ConstellationExplanation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConstellationExplanation
+//
+//  Created by MAPSSI on 06/07/2017.
+//  Copyright © 2017 KiyoungLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
