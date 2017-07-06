3c812c3a33ad62158035b90f814e94b4a8f8a05a
Kiyoung Lee <eevee300@gmail.com>
2017-07-06 10:55:52 +0900

    flight booking commit

--- FlightBooking/FlightBooking/FlightBooking/ViewController.swift
+++ FlightBooking/FlightBooking/FlightBooking/ViewController.swift
@@ -21,11 +21,35 @@ class ViewController: UIViewController {
 		returnDateButton.isHidden = !(sender as! UISwitch).isOn
 	}
 	
+	var buttonTag = 1
+	
 	@IBAction func showDatePickerAction(_ sender: Any) {
-		selectDatePicker.isHidden = false
+		if selectDatePicker.isHidden == false{
+			selectDatePicker.isHidden = true
+		} else{
+			selectDatePicker.isHidden = false
+		}
+		
+		buttonTag = (sender as! UIButton).tag
 	}
 	
+	
 	@IBAction func selectedDateAction(_ sender: Any) {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "YY-MM-d hh:mma"
+//		let dateString = formatter.string(from: selectDatePicker.date)
+		let dateString = formatter.string(from: (sender as! UIDatePicker).date)
+		
+		if buttonTag == 1 {
+			departureDateButton.setTitle(dateString, for: UIControlState())
+		} else{
+			returnDateButton.setTitle(dateString, for: UIControlState())
+		}
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		self.view.endEditing(true)
+		selectDatePicker.isHidden = true
 	}
 	
 	override func viewDidLoad() {
